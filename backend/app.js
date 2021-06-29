3ba946439dae77a1b5e7089b3bdd1fe17a180a40
Jimmy Postil <jimmy.postil@orange.fr>
2021-06-29 15:10:58 +0200

    Création des deux routes utilisateurs et de la logique métier

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 
+//Connexion à la base de données mongoDB avec mongoose
 mongoose.connect('mongodb+srv://Postil:Jimpo1599@projet6.xbckk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-{ useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
+//Initialisation des headers pour éviter les erreurs de CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -16,5 +23,10 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(bodyParser.json());
+
+//Enregistrement des routeurs
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
 
 module.exports = app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,50 @@
 const bcrypt = require('bcrypt');
-const jwt = require ('jsonwebtoken');
+const { json } = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-//Logique métiers es utilisateurs
+//Logique métiers des utilisateurs
+//Création des nouveaux utilisateurs avec signup
+exports.signup = (req, res, next) => {
+    //Hash du mot de passe à l'aide de bcrypt
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            //Création d'un nouvel utilisateur et hashage du mot de passe
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            //Sauvegarde de l'utilisateur dans la base de données
+            user.save()
+                .then(() => res.status(200).json({ message: 'Utilisateur crée !' }))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+}
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                return res.status(401).json({ error: 'Utilisateur non trouvé' });
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect' });
+                    }
+                    res.status(200).json({
+                        userId: user_id,
+                        token: jwt.sign(
+                            { userId: user.id },
+                            'RANDOM TOKEN SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+
+}
